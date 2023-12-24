755aa4db343d1790a899bf1c998c6de30f8dd8fb
Senthil-Bala <senthilkumarbala100@gmail.com>
2023-12-24 16:57:46 +0530

    modal added

--- src/ModalForm/FormOpen.jsx
+++ src/ModalForm/FormOpen.jsx
@@ -1,36 +1,41 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 
-function FormOpen() {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [phone, setPhone] = useState("");
-  const [dob, setDob] = useState("");
+function FormOpen({ onSubmit }) {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [phone, setPhone] = useState('');
+  const [dob, setDob] = useState('');
 
   const handlePhoneBlur = (e) => {
     const phoneNumber = e.target.value;
     if (phoneNumber.length < 10) {
-      alert("Invalid phone number. Please enter a 10-digit phone number.");
+      alert('Invalid phone number. Please enter a 10-digit phone number.');
     }
   };
 
   const handleDateChange = (e) => {
     const today = new Date();
     const selectedDate = new Date(e.target.value);
-    const formattedDate = selectedDate.toISOString().split("T")[0]; // Format date as YYYY-MM-DD
+    const formattedDate = selectedDate.toISOString().split('T')[0]; // Format date as YYYY-MM-DD
     setDob(formattedDate);
     if (selectedDate > today) {
-      alert("Invalid date of birth. Date of birth cannot be in the future.");
+      alert('Invalid date of birth. Date of birth cannot be in the future.');
     }
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    // Resetting the form fields
-    setName("");
-    setEmail("");
-    setPhone("");
-    setDob("");
-    // Perform form submission logic here
+    const formData = {
+      name,
+      email,
+      phone,
+      dob,
+    };
+    onSubmit(formData); // Pass form data to parent component for submission
+    setName('');
+    setEmail('');
+    setPhone('');
+    setDob('');
   };
 
   return (
--- src/ModalForm/Modal.jsx
+++ src/ModalForm/Modal.jsx
@@ -1,16 +1,34 @@
-import React,{useState} from 'react'
-import FormOpen from "./FormOpen"
+import React, { useState } from 'react';
+import FormOpen from './FormOpen';
+
 function Modal() {
-    const [isOpen, setisOpen] = useState(false)
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleCloseModal = () => {
+    setIsOpen(false);
+  };
+
+  const handleOpenModal = () => {
+    setIsOpen(true);
+  };
+
+  const handleSubmitForm = (formData) => {
+    // Perform form submission logic here using formData
+    // After successful submission:
+    setIsOpen(false); // Close modal
+  };
+
   return (
     <div className="modal">
-        <h1>User Details Form</h1>
-        <button onClick={()=>{
-            setisOpen(!isOpen)
-        }}>Open Form</button>
-        {isOpen ? <FormOpen /> : null}
+      <h1>User Details Form</h1>
+      <button onClick={handleOpenModal}>Open Form</button>
+      {isOpen && (
+        <div className="modal-content">
+          <FormOpen onSubmit={handleSubmitForm} />
+        </div>
+      )}
     </div>
-  )
+  );
 }
 
-export default Modal
+export default Modal;
